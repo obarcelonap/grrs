c6d0d8fe817ac395e7b9d3d12a6a3a88b561cae9
Oriol Barcelona <obarcelonap@gmail.com>
2021-03-26 12:43:11 +0100

    Add testing.

---
+++ tests/cli.rs
@@ -0,0 +1,35 @@
+use std::io::{Write};
+// Used for writing assertions
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+// Add methods on commands
+use predicates::prelude::*;
+use tempfile::NamedTempFile;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg("foobar")
+        .arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("test\nAnother test"));
+
+    Ok(())
+}
